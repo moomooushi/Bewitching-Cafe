f2f174f77d4db9a2cdd12eda4e2efd4b25183c93
Pianja <94577886+Pianja@users.noreply.github.com>
2023-04-06 13:38:22 +1000

    lil

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -12,7 +12,7 @@ public class Tutorial : MonoBehaviour
 
     private void Update()
     {
-        if (stage == 0)//introduce orders
+        if (stage == 1)//introduce orders
         {
             focusWindow.SetActive(true);
             clickSpot.SetActive(true);
@@ -20,32 +20,36 @@ public class Tutorial : MonoBehaviour
             narrator.GetComponentInChildren<TextMeshProUGUI>().text = "Click the letter to accept an order";//orders position Vector2(341.299988,179.899994)
             //if clickspot clicked stage++;
         }
-        else if (stage == 1)//introduce potion making
+        else if (stage == 2)//introduce potion making
         {
             focusWindow.transform.localPosition = new Vector2(250, 10);//recipebook location
             clickSpot.transform.localPosition = new Vector2();
             focusWindow.SetActive(false);
             narrator.GetComponentInChildren<TextMeshProUGUI>().text = "It looks like they want 1 healing potion, click on the X to close";
         }
-        else if (stage == 2)
+        else if (stage == 3)
         {
             focusWindow.transform.localPosition = new Vector2(250, 10);//recipebook location
             clickSpot.transform.localPosition = new Vector2();
             narrator.GetComponentInChildren<TextMeshProUGUI>().text = "Click the books to look at recipes";
             focusWindow.SetActive(true);
         }
-        else if (stage == 3)
+        else if (stage == 4)
         {
             focusWindow.transform.localPosition = new Vector2(250, 10);//recipebook location
             clickSpot.transform.localPosition = new Vector2();
             narrator.GetComponentInChildren<TextMeshProUGUI>().text = "This is the potion we'll be making, we'll need a <color=\"red\"> blue berry and Carnelian";
             focusWindow.SetActive(false);
         }
-        else if (stage == 4)
+        else if (stage == 5)
         {
 
         }
 
+    }
+    public void NextStage()
+    {
+        stage++;
     }
 
 }
