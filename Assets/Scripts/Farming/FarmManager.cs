8992ddc1874c882b6540945b6cfe3903c0a8cd31
Thomas Graham <thomasgraham874@gmail.com>
2023-04-12 17:13:18 +1000

    Started working on Farming scene -Lily

---
+++ Assets/Scripts/Farming/FarmManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FarmManager : MonoBehaviour
+{
+    public PlantItem selectPlant;
+    public bool isPlanting = false;
+    public int money = 100;
+    public Text moneyTxt;
+
+    public Color buyColor = Color.green;
+    public Color cancelColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SelectPlant(PlantItem newPlant)
+    {
+        if(selectPlant == newPlant)
+        {
+            selectPlant.btnImage.color = buyColor;
+            selectPlant.btnText.text = "Buy";
+            selectPlant = null;
+            isPlanting = false;
+        }
+        else
+        {
+            if(selectPlant != null)
+            {
+                selectPlant.btnImage.color = buyColor;
+                selectPlant.btnText.text = "Buy";
+            }
+            selectPlant = newPlant;
+            selectPlant.btnImage.color = cancelColor;
+            selectPlant.btnText.text = "Cancel";
+            isPlanting = true;
+        }
+    }
+
+    public void Transaction(int value)
+    {
+        money += value;
+        moneyTxt.text = "$" + money;
+    }
+}
---
+++ Assets/Scripts/Farming/PlotManager.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotManager : MonoBehaviour
+{
+
+    bool isPlanted = false;
+    SpriteRenderer plant;
+    BoxCollider2D plantCollider;
+
+    int plantStage = 0;
+    float timer;
+
+    public Color availableColor = Color.green;
+    public Color unavailableColor = Color.red;
+
+    SpriteRenderer plot;
+
+    PlantObject selectedPlant;
+
+    FarmManager fm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plant = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        plantCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+        fm = transform.parent.GetComponent<FarmManager>();
+        plot = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlanted)
+        {
+            timer -= Time.deltaTime;
+            if(timer < 0 && plantStage < selectedPlant.plantStages.Length - 1)
+            {
+                timer = selectedPlant.timeBtwStages;
+                plantStage++;
+                UpdatePlant();
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlanted)
+        {
+            //We want to harvest if crop is planted
+            if(plantStage == selectedPlant.plantStages.Length - 1 && !fm.isPlanting)
+            {
+                Harvest();
+            }
+        }
+        else if(fm.isPlanting && fm.selectPlant.plant.buyPrice <= fm.money)
+        {
+            Plant(fm.selectPlant.plant);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (fm.isPlanting)
+        {
+            if(isPlanted || fm.selectPlant.plant.buyPrice > fm.money)
+            {
+                //Can't buy
+                plot.color = unavailableColor;
+            }
+            else
+            {
+                //Can buy
+                plot.color = availableColor;
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        plot.color = Color.white;
+    }
+
+    void Harvest()
+    {
+        Debug.Log("Harvested");
+        isPlanted = false;
+        plant.gameObject.SetActive(false);
+        fm.Transaction(selectedPlant.sellPrice);
+    }
+
+    void Plant(PlantObject newPlant)
+    {
+        selectedPlant = newPlant;
+        Debug.Log("Planted");
+        isPlanted = true;
+
+        fm.Transaction(-selectedPlant.buyPrice);
+
+        plantStage = 0;
+        UpdatePlant();
+        timer = selectedPlant.timeBtwStages;
+        plant.gameObject.SetActive(true);
+    }
+
+    void UpdatePlant()
+    {
+        plant.sprite = selectedPlant.plantStages[plantStage];
+        plantCollider.size = plant.sprite.bounds.size;
+        plantCollider.offset = new Vector2(0, plant.bounds.size.y/2);
+    }
+}
